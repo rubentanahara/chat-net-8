02a2e2c88a15c7564f3eff5ad4961da66c99e0cf
rubentanahara <ruben.mtz.tanahara@outlook.com>
2025-02-22 05:23:13 -0800

    Adding gitignore and editor config

--- src/Application/ChatSystem.Application/obj/Debug/net8.0/ChatSystem.Application.AssemblyInfo.cs
+++ src/Application/ChatSystem.Application/obj/Debug/net8.0/ChatSystem.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatSystem.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907dae751cab4f7c3b9311a363747720d94e1ff8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatSystem.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatSystem.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Infrastructure/ChatSystem.Infrastructure/obj/Debug/net8.0/ChatSystem.Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/ChatSystem.Infrastructure/obj/Debug/net8.0/ChatSystem.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907dae751cab4f7c3b9311a363747720d94e1ff8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
